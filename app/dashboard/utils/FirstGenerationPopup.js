b56bb7c8f4d5194d2fa49f97b20e60067f8e7423
wesiudev <wesselpawel@wp.pl>
2023-04-24 21:30:51 +0200

    empty backpack scenerio

--- app/dashboard/utils/FirstGenerationPopup.js
+++ app/dashboard/utils/FirstGenerationPopup.js
@@ -1,11 +1,8 @@
 import React from "react";
-import "./ImageContainer.css";
-import Image from "next/image";
-import Link from "next/link";
 import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 
-export default function ImageContainer({
+export default function FirstGenerationPopup({
   isLoading,
   hasImage,
   imageUrl,
@@ -16,10 +13,10 @@ export default function ImageContainer({
   return (
     <>
       {hasImage && (
-        <div className="z-50 fixed h-screen w-screen top-0 left-0  flex items-center justify-center bg-black bg-opacity-80 ">
-          <div className="h-[90%] w-[95vw] bg-purple-950  rounded-xl px-6 lg:px-24 flex flex-col justify-evenly">
+        <div className="z-50 fixed h-screen w-screen top-0 left-0 flex items-center justify-center bg-black bg-opacity-80 ">
+          <div className="h-max sm:w-max w-[95%] bg-purple-950 rounded-xl py-6 px-3 sm:px-12 flex flex-col justify-evenly">
             <div className="w-full flex justify-center">
-              <strong className="bg-gradient-to-r from-purple-600 to-rose-600 bg-clip-text text-transparent text-3xl sm:text-4xl py-2">
+              <strong className="bg-gradient-to-r from-purple-600 to-rose-600 bg-clip-text text-transparent text-3xl sm:text-4xl pb-2">
                 Congratulations!
               </strong>
             </div>
@@ -28,7 +25,7 @@ export default function ImageContainer({
               <strong className="text-purple-600"> canva</strong>!
             </div>
 
-            <div className="w-3/4 flex items-center justify-center mx-auto">
+            <div className="w-[90%] sm:w-3/4 flex items-center justify-center mx-auto">
               {!isLoading && (
                 <motion.img
                   initial={{ opacity: 0.5, scale: 0.9 }}
@@ -36,13 +33,13 @@ export default function ImageContainer({
                   transition={{ duration: 1.5 }}
                   src={imageUrl}
                   alt={userPrompt}
-                  className="lg:w-[450px] lg:h-[450px] py-5 "
+                  className="w-full sm:min-w-[350px] sm:h-[350px] py-5 "
                   onLoad={() => setImageLoaded(true)}
                   style={{ display: imageLoaded ? "block" : "none" }}
                 />
               )}
               {!imageLoaded && (
-                <div className="bg-purple-800 h-full w-full text-2xl text-gray-50 flex justify-center items-center text-center mx-auto">
+                <div className="bg-purple-800 h-[33vh] w-full text-2xl text-gray-50 flex justify-center items-center text-center mx-auto">
                   <Typewriter
                     options={{
                       loop: true,
@@ -69,7 +66,7 @@ export default function ImageContainer({
               )}
             </div>
             <div className="w-full flex justify-center ">
-              <button className="w-max bg-green-800 hover:bg-green-700 px-5 py-6 sm:py-3 rounded-md text-gray-100 text-xl sm:text-3xl ">
+              <button className="w-max bg-green-800 hover:bg-green-700 py-3 rounded-md text-gray-100 text-2xl px-12 my-5">
                 Okay!
               </button>
             </div>
