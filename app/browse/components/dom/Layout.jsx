cdeec736775c8fef3e31ff6c0708414bd5af559d
wesiudev <wesselpawel@wp.pl>
2023-05-22 21:19:27 +0200

    fix

--- app/browse/components/dom/Layout.jsx
+++ app/browse/components/dom/Layout.jsx
@@ -1,38 +1,41 @@
-import { useRef } from 'react'
-import dynamic from 'next/dynamic'
-const Scene = dynamic(() => import('../../components/canvas/Scene'), { ssr: false })
+"use client";
+import { useRef } from "react";
+import dynamic from "next/dynamic";
+const Scene = dynamic(() => import("../../components/canvas/Scene"), {
+  ssr: false,
+});
 
 const Layout = ({ children }) => {
-  const ref = useRef()
+  const ref = useRef();
 
   return (
     <div
       ref={ref}
       style={{
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        overflow: 'auto',
-        touchAction: 'auto',
-        left:"0px",
-        top:"0px"
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        overflow: "auto",
+        touchAction: "auto",
+        left: "0px",
+        top: "0px",
       }}
     >
       {children}
       <Scene
         style={{
-          position: 'fixed',
+          position: "fixed",
           top: 0,
           left: 0,
-          width: '100vw',
-          height: '100vh',
-          pointerEvents: 'none',
+          width: "100vw",
+          height: "100vh",
+          pointerEvents: "none",
         }}
         eventSource={ref}
-        eventPrefix='client'
+        eventPrefix="client"
       />
     </div>
-  )
-}
+  );
+};
 
-export { Layout }
+export { Layout };
