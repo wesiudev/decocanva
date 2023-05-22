fe5a8488ed171770fb41e9c3860778f39626b53d
wesiudev <wesselpawel@wp.pl>
2023-05-22 21:13:57 +0200

    fix

--- app/browse/components/ImagesGrid.tsx
+++ app/browse/components/ImagesGrid.tsx
@@ -1,48 +1,48 @@
-"use client";
-import capitalizeString from "@/app/utils/CapitalizeString";
-import truncate from "@/app/utils/CutString";
-import { ImageProps, ImagesArray } from "@/types";
-import Image from "next/image";
-import { useState } from "react";
-import Canvas3D from "./Canvas3D";
-import React from "react";
-import { Layout } from "../components/dom/Layout";
-export default function ImagesGrid({ images }: { images: ImageProps[] }) {
-  const [currentImage, setCurrentImage] = useState<ImageProps>();
-  return (
-    <div className="relative">
-      {/* {currentImage ? (
-        <div className="fixed left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] h-[80vh] rounded-lg z-50">
-          <div className="w-[90vw] lg:w-[80vw] h-[50vh] overflow-y-scroll scrollbar">
-            <Layout>
-              <Canvas3D
-                image={currentImage}
-                setCurrentImage={setCurrentImage}
-              />
-            </Layout>
-          </div>
-        </div>
-      ) : null} */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 w-full gap-3 mx-auto mt-3">
-        {images?.map((image: ImageProps, idx: number) => (
-          <div key={idx} className="relative">
-            <div className="relative group">
-              <Image
-                priority
-                width={512}
-                height={512}
-                src={image.src}
-                alt=""
-                key={idx}
-                onClick={() => setCurrentImage(image)}
-              />
-              <div className="absolute bottom-0 left-0 p-2 bg-black text-white w-full opacity-0 group-hover:opacity-100 ease-in duration-200">
-                {truncate(capitalizeString(image.prompt)!, 38)}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+// "use client";
+// import capitalizeString from "@/app/utils/CapitalizeString";
+// import truncate from "@/app/utils/CutString";
+// import { ImageProps, ImagesArray } from "@/types";
+// import Image from "next/image";
+// import { useState } from "react";
+// import Canvas3D from "./Canvas3D";
+// import React from "react";
+// import { Layout } from "../components/dom/Layout";
+// export default function ImagesGrid({ images }: { images: ImageProps[] }) {
+//   const [currentImage, setCurrentImage] = useState<ImageProps>();
+//   return (
+//     <div className="relative">
+//       {/* {currentImage ? (
+//         <div className="fixed left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] h-[80vh] rounded-lg z-50">
+//           <div className="w-[90vw] lg:w-[80vw] h-[50vh] overflow-y-scroll scrollbar">
+//             <Layout>
+//               <Canvas3D
+//                 image={currentImage}
+//                 setCurrentImage={setCurrentImage}
+//               />
+//             </Layout>
+//           </div>
+//         </div>
+//       ) : null} */}
+//       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 w-full gap-3 mx-auto mt-3">
+//         {images?.map((image: ImageProps, idx: number) => (
+//           <div key={idx} className="relative">
+//             <div className="relative group">
+//               <Image
+//                 priority
+//                 width={512}
+//                 height={512}
+//                 src={image.src}
+//                 alt=""
+//                 key={idx}
+//                 onClick={() => setCurrentImage(image)}
+//               />
+//               <div className="absolute bottom-0 left-0 p-2 bg-black text-white w-full opacity-0 group-hover:opacity-100 ease-in duration-200">
+//                 {truncate(capitalizeString(image.prompt)!, 38)}
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
--- app/browse/components/SSRImagesGrid.tsx
+++ app/browse/components/SSRImagesGrid.tsx
@@ -1,10 +1,10 @@
 import { store } from "@/common/redux/store";
-import ImagesGrid from "./ImagesGrid";
+// import ImagesGrid from "./ImagesGrid";
 
 function SSRImageGrid() {
   return (
     <div>
-      <ImagesGrid images={store.getState().images.browseImages} />
+      {/* <ImagesGrid images={store.getState().images.browseImages} /> */}
     </div>
   );
 }
