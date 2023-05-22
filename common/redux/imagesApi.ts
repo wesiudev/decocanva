8de3548ab9622cff32370c47f8b1c69eca8dc0aa
wesiudev <wesselpawel@wp.pl>
2023-05-22 11:44:00 +0200

    api

--- common/redux/imagesApi.ts
+++ common/redux/imagesApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 export const browseImages = createApi({
   reducerPath: "browseImages",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NODE_ENV  }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://www.decocanva.com"  }),
   tagTypes: ["pokemon"],
   endpoints: (builder) => ({
     getImages: builder.query<ImageProps[], void>({
